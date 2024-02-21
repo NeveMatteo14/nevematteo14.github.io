7cd7696553d835067efb896098db4f4c6319eada
Matthew DellaNeve <mjdellaneve@gmail.com>
2024-02-21 16:17:32 -0500

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// const moonRustModal = document.getElementById("moonRust");
+
+// let moonRustDemo;
+
+// moonRustModal.addEventListener("show.bs.modal", (event) => {
+//   moonRustDemo = new YT.Player("moonRustDemo");
+// });
+
+// moonRustModal.addEventListener("hidden.bs.modal", (event) => {
+//   moonRustDemo.stopVideo();
+// });
